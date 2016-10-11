e71c6fc7c278ba4ea56b7102367a4791ba6f85a6
Normandes Junior <normandesjr@gmail.com>
2016-10-11 11:23:15 -0300

    Controller added

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -1,10 +1,21 @@
 package com.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
 public class DemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String home() {
+        return "Hello AlgaWorks!";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
